c6ea38fc576c918e74ef6fcd54d5840587ac637e
oruemong <orudevgame>
2026-04-03 22:34:49 +0900

    StageResultUI 수정
    스크립트 내용 추가 및 수정

--- Four-Tiger-Sword/Assets/_Project/Scripts/UI/StageResultUI.cs
+++ Four-Tiger-Sword/Assets/_Project/Scripts/UI/StageResultUI.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StageResultUI : MonoBehaviour
+public class PlayerStatusHUD : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    [Header("Bars")]
+    [SerializeField] private Slider hpBar;  // HP 슬라이더
+    [SerializeField] private Slider spBar;  // SP 슬라이더
+
+    private PlayerStatManager statManager;
+
+    private void Start()
+    {        // 씬에서 PlayerStatManager 찾기
+        statManager = FindFirstObjectByType<PlayerStatManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (statManager == null) return;
+
+        // 0~1 비율로 바 업데이트
+        hpBar.value = statManager.CurrentHP / statManager.MaxHP;
+        spBar.value = statManager.CurrentSP / statManager.MaxSP;
     }
 }
